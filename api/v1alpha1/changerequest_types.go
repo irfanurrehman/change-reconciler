e90630f04c298b90d37f63499da240321573b2c3
irfanurrehman <irfan.rehman@turbonomic.com>
2021-10-26 20:44:23 +1100

    Update API fields and generate code

--- api/v1alpha1/changerequest_types.go
+++ api/v1alpha1/changerequest_types.go
@@ -17,19 +17,48 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// ChangeRequestType defines types of ChangeRequest
+type ChangeRequestType string
+
+const (
+	// Type defines type name of GitHub repository
+	ChangeRequestGitHub = "github"
+
+	// More types will be added here
+)
 
 // ChangeRequestSpec defines the desired state of ChangeRequest
 type ChangeRequestSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of ChangeRequest. Edit changerequest_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// A string representation of the ChangeRequesr type. Valid values include:
+	// `GitHub`,`github`
+	// +kubebuilder:validation:Enum={GitHub,github}
+	Type ChangeRequestType `json:"type"`
+
+	// Pathname is the remote URL which is the source of truth.
+	// The CR controller will decipher the details from the path.
+	// eg. https://github.com/turbonomic/kubeturbo/blob/master/deploy/kubeturbo_yamls/step5_turbo_kubeturboDeploy.yaml
+	// TODO: Figure out if this is sufficient to pinpoint a given configuration
+	// If not, then a separate field specifying the repo specific configuration file path can be added.
+	Pathname string `json:"pathname"`
+
+	// Payload is the yaml structured resource data that needs to be updated
+	// at the path in the source of truth repo.
+	// The reconciler will expect that the git repo stores yamls and will
+	// diff the spec of the retrieved yaml with the source of truth path to determine
+	// if an update is needed.
+	Payload string `json:"payload"`
+
+	// For a `github` as the source of truth, this
+	// can be used to reference a Secret which contains the credentials for
+	// authentication, i.e. `user` and `accessToken`.
+	// +optional
+	SecretRef *corev1.ObjectReference `json:"secretRef,omitempty"`
 }
 
 // ChangeRequestStatus defines the observed state of ChangeRequest
