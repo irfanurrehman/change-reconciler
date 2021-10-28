ed95df9fa925e3feb679a340b6c7eba535a103d2
irfanurrehman <irfan.rehman@turbonomic.com>
2021-10-28 23:07:18 +1100

    Update API spec

--- api/v1alpha1/changerequest_types.go
+++ api/v1alpha1/changerequest_types.go
@@ -18,6 +18,7 @@ package v1alpha1
 
 import (
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -25,34 +26,75 @@ import (
 type ChangeRequestType string
 
 const (
-	// Type defines type name of GitHub repository
-	ChangeRequestGitHub = "github"
+	// GitHub repository
+	ChangeRequestGitHub ChangeRequestType = "github"
 
 	// More types will be added here
 )
+
+type ChangeRequestState string
+
+const (
+	StateInitial   ChangeRequestState = ""
+	StateUpdating  ChangeRequestState = "Updating"
+	StateCompleted ChangeRequestState = "Complete"
+	StateFailed    ChangeRequestState = "Failed"
+)
+
+type ChangeRequestPushMode string
+
+const (
+	// Reconciler with try to raise a PR with this mode
+	PushModeRequestApproval ChangeRequestPushMode = "requestapproval"
+
+	// Reconciler with try to update the file in place in this mode
+	PushModeDirect ChangeRequestPushMode = "direct"
+)
+
+type PatchItem struct {
+	Op   string `json:"op,omitempty"`
+	Path string `json:"path"`
+	// We needed an Interface{} typed here, but controller-gen doesn't support that.
+	// We use a solution listed at https://github.com/kubernetes-sigs/controller-tools/pull/126#issuecomment-707233008.
+	// For more reference:
+	// https://github.com/kubernetes-sigs/controller-tools/pull/126#issuecomment-630764976
+	// https://github.com/kubernetes-sigs/controller-tools/issues/294#issuecomment-518379253
+	Value apiextensionsv1.JSON `json:"value,omitempty"`
+}
 
 // ChangeRequestSpec defines the desired state of ChangeRequest
 type ChangeRequestSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// A string representation of the ChangeRequesr type. Valid values include:
+	// A string representation of the ChangeRequest type. Valid values include:
 	// `GitHub`,`github`
 	// +kubebuilder:validation:Enum={GitHub,github}
 	Type ChangeRequestType `json:"type"`
 
-	// Pathname is the remote URL which is the source of truth.
-	// The CR controller will decipher the details from the path.
-	// eg. https://github.com/turbonomic/kubeturbo/blob/master/deploy/kubeturbo_yamls/step5_turbo_kubeturboDeploy.yaml
-	// TODO: Figure out if this is sufficient to pinpoint a given configuration
-	// If not, then a separate field specifying the repo specific configuration file path can be added.
-	Pathname string `json:"pathname"`
+	// A string representation of the ChangeRequesr type. Valid values include:
+	// `requestapproval`,`github`
+	// +kubebuilder:validation:Enum={requestapproval,direct}
+	// +optional
+	Mode ChangeRequestPushMode `json:"mode"`
 
-	// Payload is the yaml structured resource data that needs to be updated
-	// at the path in the source of truth repo.
-	// The reconciler will expect that the git repo stores yamls and will
-	// diff the spec of the retrieved yaml with the source of truth path to determine
-	// if an update is needed.
-	Payload string `json:"payload"`
+	// Source is the remote URL which is the source of truth.
+	// eg. https://github.com/turbonomic/kubeturbo
+	Source string `json:"source"`
+
+	// FilePath is the path of file to be updated in the repo.
+	// eg. deploy/kubeturbo_yamls/step5_turbo_kubeturboDeploy.yaml
+	FilePath string `json:"filePath"`
+
+	// Branch optionally specifies the branch to which the file will be updated.
+	// If omitted the update will be pushed to the main branch.
+	// +optional
+	Branch string `json:"branch"`
+
+	// PatchItems are a list of jsonpatch style patches that will be applied
+	// to the spec from the file at the source of truth, eg. a git repo.
+	// After the patch is applied the the file will be pushed back to the repo
+	// using the policy specified in "Mode".
+	PatchItems []PatchItem `json:"patchItems"`
 
 	// For a `github` as the source of truth, this
 	// can be used to reference a Secret which contains the credentials for
@@ -63,8 +105,9 @@ type ChangeRequestSpec struct {
 
 // ChangeRequestStatus defines the observed state of ChangeRequest
 type ChangeRequestStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	State ChangeRequestState `json:"state"`
+	// TODO: A field which can identify the created or to be created PR uniquely
+	// can help in possible multiple reconciles for the same ChangeRequest
 }
 
 //+kubebuilder:object:root=true
